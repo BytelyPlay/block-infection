5c0e24dad7f955ef977784caebbe5a52159a73c6
BytelyPlay <100842216+Killerof6@users.noreply.github.com>
2025-07-31 18:41:15 +0200

    idk what uncommitted changes i have

--- src/main/java/org/hyperoil/blockinfection/Blocks/InfectionBlock.java
+++ src/main/java/org/hyperoil/blockinfection/Blocks/InfectionBlock.java
@@ -1,18 +1,23 @@
 package org.hyperoil.blockinfection.Blocks;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.PushReaction;
+import net.minecraft.world.level.redstone.Orientation;
 import net.minecraft.world.phys.AABB;
 import org.hyperoil.blockinfection.Utils.BlocksHelper;
 import org.hyperoil.blockinfection.hyperoil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.util.concurrent.ThreadLocalRandom;
@@ -43,9 +48,14 @@ public class InfectionBlock extends Block {
     }
 
     @Override
-    protected void onPlace(BlockState state, Level level, BlockPos pos, BlockState oldState, boolean movedByPiston) {
-        super.onPlace(state, level, pos, oldState, movedByPiston);
-
+    protected void onPlace(@NotNull BlockState state, Level level, @NotNull BlockPos pos, @NotNull BlockState oldState, boolean movedByPiston) {
         level.scheduleTick(pos, this, ThreadLocalRandom.current().nextInt(2, 6));
+    }
+
+    @Override
+    protected void neighborChanged(BlockState state, Level level, BlockPos pos, Block neighborBlock, @Nullable Orientation orientation, boolean movedByPiston) {
+        if (level instanceof ServerLevel) {
+            tick(state, (ServerLevel) level, pos, level.getRandom());
+        }
     }
 }
