ae50b5ec46123c9c38ee9f0598801d5ddfe44f28
BytelyPlay <100842216+Killerof6@users.noreply.github.com>
2025-08-04 15:07:26 +0200

    It is now theoretically playable just going to texture the uncharger grower block and going to update the readme to tell you how to play it and yeah i'll push it to master and make the first release

--- src/main/java/org/hyperoil/blockinfection/Blocks/UnchargerGrowerBlock.java
+++ src/main/java/org/hyperoil/blockinfection/Blocks/UnchargerGrowerBlock.java
@@ -9,6 +9,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
@@ -26,8 +27,14 @@ public class UnchargerGrowerBlock extends Block {
     @Override
     protected void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
         int growth = state.getValue(GROWTH);
-        if (growth > 8) return;
-        state.setValue(GROWTH, growth + 1);
+        if (growth >= 8) return;
+        BlockState afterState = state.setValue(GROWTH, growth + 1);
+        level.setBlock(pos, afterState, Block.UPDATE_CLIENTS);
+    }
+
+    @Override
+    protected void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        System.out.println("a");
     }
 
     @Override
@@ -37,18 +44,18 @@ public class UnchargerGrowerBlock extends Block {
 
     @Override
     protected @NotNull InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hitResult) {
-        return rightClicked(state, player);
+        return rightClicked(state, player, level, pos);
     }
 
     @Override
     protected InteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
-        return rightClicked(state, player);
+        return rightClicked(state, player, level, pos);
     }
 
-    private @NotNull InteractionResult rightClicked(BlockState state, Player player) {
+    private @NotNull InteractionResult rightClicked(BlockState state, Player player, Level level, BlockPos pos) {
         if (state.getValue(GROWTH) >= 8) {
             if (player.getInventory().add(new ItemStack(ItemsHelper.UNCHARGING_STAFF.get()))) {
-                state.setValue(GROWTH, 1);
+                level.setBlock(pos, Blocks.AIR.defaultBlockState(), Block.UPDATE_CLIENTS);
             }
         }
         return InteractionResult.SUCCESS;
